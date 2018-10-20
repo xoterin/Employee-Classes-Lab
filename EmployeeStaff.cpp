10cdc065923d6bbd6966908dad2bb394f1c60c46
xoterin <44329085+xoterin@users.noreply.github.com>
2018-10-20 19:46:07 -0400

    Create EmployeeStaff.cpp

---
+++ EmployeeStaff.cpp
@@ -0,0 +1,32 @@
+// EmployeeStaff definition
+#include <iostream>
+#include <string>
+#include "EmployeeStaff.h"
+using namespace std;
+
+// Default constructor
+EmployeeStaff::EmployeeStaff() {
+   managerName = "";
+}
+
+// ***********************************************************************
+
+// Constructor with parameters
+EmployeeStaff::EmployeeStaff(string reportsTo) {
+   managerName = reportsTo;
+}
+
+// ***********************************************************************
+
+// Get the name of the manager
+string EmployeeStaff::GetManagerName() {
+   return managerName;
+}
+
+// ***********************************************************************
+
+// FIXME: Override the EmployeePerson class' printInfo function with a
+// FIXME: printInfo function to print all staff data members.
+void EmployeeStaff::PrintInfo() {
+      cout << "Name: " << fullName << ", Department: " << departmentCode << ", Birthday: " << birthday << ", Salary: " << annualSalary << ", Manager: " << managerName << endl;
+}
