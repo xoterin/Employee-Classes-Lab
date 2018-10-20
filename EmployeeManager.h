45a772fb88abb27fdb48f04b752023830ee1ac4c
xoterin <44329085+xoterin@users.noreply.github.com>
2018-10-20 19:10:48 -0400

    Create EmployeeManager.h

---
+++ EmployeeManager.h
@@ -0,0 +1,19 @@
+#ifndef EMPLOYEEMANAGER_H
+#define EMPLOYEEMANAGER_H
+
+#include <iostream>
+#include <string>
+#include "EmployeePerson.h"
+using namespace std;
+
+class EmployeeManager : public EmployeePerson {
+   public:
+      EmployeeManager();
+      EmployeeManager(int nManaged);
+      int GetNumManaged();
+      void PrintInfo();
+   private:
+      int numManaged;
+};
+
+#endif
